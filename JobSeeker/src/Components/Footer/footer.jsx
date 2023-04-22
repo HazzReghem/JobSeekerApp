99f0ea36b92ffd6d1172d53d62cb375776810fd9
Thibault Roussel <thibault.roussel@gmx.com>
2023-04-22 18:28:48 +0200

    footer added

--- JobSeeker/src/Components/Footer/footer.jsx
+++ JobSeeker/src/Components/Footer/footer.jsx
@@ -1,9 +1,59 @@
-import React from "react"
+import React from 'react'
+import './footer.scss'
 
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
+  return (
+    <div className="footer">
+      <div>
+        <div className="logoDiv">
+          <h1 className="logo">
+            <strong>Job</strong>Search
+          </h1>
+        </div>
+        <p className="footerP">
+          We always make our seekers find the best jobs, and our companies the
+          best candidates
+        </p>
+      </div>
+
+      <div className="footerInfo">
+        <span>Company</span>
+        <ul>
+          <li>About us</li>
+          <li>Features</li>
+          <li>News</li>
+          <li>FAQ</li>
+        </ul>
+      </div>
+
+      <div className="footerInfo">
+        <span>Ressources</span>
+        <ul>
+          <li>Account</li>
+          <li>Support Center</li>
+          <li>Feedback</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div className="footerInfo">
+        <span>Support</span>
+        <ul>
+          <li>Events</li>
+          <li>Promo</li>
+          <li>Req Demo</li>
+          <li>Careers</li>
+        </ul>
+      </div>
+
+      <div className="infoPerso">
+        <span className="contactInfo">Contact Info</span>
+        <div className="mailDiv">
+          <small>thibault.roussel@gmx.com</small>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
--- JobSeeker/src/Components/Value/value.jsx
+++ JobSeeker/src/Components/Value/value.jsx
@@ -1,9 +1,64 @@
-import React from "react"
+import React from 'react'
+import './value.scss'
+
+// Imported Images ====>
+import value_img from '../../Assets/value1.jpg'
+import value_img2 from '../../Assets/value2.webp'
+import value_img3 from '../../Assets/value3.jpg'
 
 const Value = () => {
-    return (
-        <div>Value</div>
-    )
+  return (
+    <div className="valueContainer">
+      <h1>The value that holds us true and to account</h1>
+      <div className="valueDiv">
+        <div className="singleGrid">
+          <div>
+            <div className="imgDiv">
+              <img src={value_img} alt="" />
+            </div>
+
+            <span>Simplicity</span>
+          </div>
+
+          <p>
+            Things being made beautiful are at the heart of everything we do.
+          </p>
+        </div>
+
+        <div className="singleGrid">
+          <div>
+            <div className="imgDiv">
+              <img src={value_img2} alt="" />
+            </div>
+
+            <span>Humanity</span>
+          </div>
+
+          <p>
+            We believe in making things better for everyone, even if just by a
+            little bit !
+          </p>
+        </div>
+
+        <div className="singleGrid">
+          <div>
+            <div className="imgDiv">
+              <img src={value_img3} alt="" />
+            </div>
+
+            <span>Transparency</span>
+          </div>
+
+          <p>
+            We work on the basis of creating trust, which can be nurtered
+            through authenticity and transparency.
+          </p>
+        </div>
+      </div>
+
+      <div className="card"></div>
+    </div>
+  )
 }
 
 export default Value
